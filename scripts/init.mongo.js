aeebabb2c2f2f7f83542e45587cea3fea693b6f6
Hitendra Quessou <sanjay_quessou@yahoo.co.uk>
2022-01-30 20:55:31 +0000

    Chapter 6. Writing to MongoDB

---
+++ scripts/init.mongo.js
@@ -0,0 +1,49 @@
+/*
+* Run using the mongo shell. For remote databases, ensure that the
+* connection string is supplied in the command line. For example:
+* localhost:
+*   mongo issuetracker scripts/init.mongo.js
+* Altas:
+*   mongo mongodb+srv://user:pwd@xxx.mongodb.net/issuetracker
+    scripts/init.mongo.js
+* MLab:
+* mongo mongodb://user:pwd@xxx.mlab.com:33533/issuetracker
+    scripts/init.mongo.js
+*/
+
+db.issues.remove({});
+
+const issuesDB = [
+    {
+      id: 1, status: 'New', owner: 'Ravan', effort: 5,
+      created: new Date('2018-08-15'), due: undefined,
+      title: 'Error in console when clicking Add',
+    },
+  
+    {
+      id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+      created: new Date('2018-08-16'), due: new Date('2018-08-30'),
+      title: 'Missing bottom border on panel',
+  
+    },
+    {
+      id: 3, status: 'New', owner: 'Sanjay', effort: 7,
+      created: new Date('2021-07-7'), due: new Date('2021-07-12'),
+      title: 'PRO MERN FULL STACK',
+    },
+  ];
+
+ db.issues.insertMany(issuesDB);
+ const count = db.issues.count();
+ print('Inserted', count, 'issues');
+
+
+ db.counters.remove({ _id: 'issues' });
+ db.counters.insert({ _id: 'issues', current: count });
+ db.issues.createIndex({ id:1 }, { unique: true });
+ db.issues.createIndex({ status: 1 });
+ db.issue.createIndex({ owner: 1 });
+ db.issues.createIndex({ created: 1 });
+ 
+ 
+
--- scripts/trymongo.js
+++ scripts/trymongo.js
@@ -1,59 +1,75 @@
-"use strict";
+const { MongoClient } = require('mongodb');
 
-var _require = require('mongodb'),
-    MongoClient = _require.MongoClient;
+const url = 'mongodb://localhost/issuetracker';
 
-var url = 'mongodb://localhost/issuetracker'; //Atlas URL - replace UUU with user, PPP with password, XXX with hostname
+//Atlas URL - replace UUU with user, PPP with password, XXX with hostname
 // const url = 'mongodb+srv://UUU:PPP@cluster0-XXX.mongodb.net/issuetracker?retryWrites=true';
+
 // mLab URL - replace UUU with user, PPP with password, XXX with hostname
 // const url = 'mongodb://UUU:PPP@XXX.mlab.com:33533/issuetracker';
 
 function testWithCallbacks(callback) {
-  console.log('\n--- testWithCallbacks ---');
-  var client = new MongoClient(url, {
-    useNewUrlParser: true
-  });
-  client.connect(function (err, client) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    console.log('Connected to MongoDB');
-    var db = client.db();
-    var collection = db.collection('employees');
-    var employee = {
-      id: 1,
-      name: 'A. Callback',
-      age: 23
-    };
-    collection.insertOne(employee, function (err, result) {
-      if (err) {
-        client.close();
-        callback(err);
-        return;
-      }
-
-      console.log('Result of insert:\n', result.insertedId);
-      collection.find({
-        _id: result.insertedId
-      }).toArray(function (err, docs) {
+    console.log('\n--- testWithCallbacks ---');
+    const client = new MongoClient(url, { useNewUrlParser: true });
+    client.connect(function(err, client) {
         if (err) {
-          client.close();
-          callback(err);
-          return;
+            callback(err);
+            return;
         }
+        console.log('Connected to MongoDB');
 
-        console.log('Result of find:\n', docs);
-        client.close();
-        callback(err);
-      });
+        const db = client.db();
+        const collection = db.collection('employees');
+
+        const employee = { id:1, name: 'A. Callback', age: 23 };
+        collection.insertOne(employee, function(err, result) {
+            if (err) {
+                client.close();
+                callback(err);
+                return;
+            }
+            console.log('Result of insert:\n', result.insertedId);
+            collection.find({_id: result.insertedId})
+                .toArray(function(err, docs) {
+                    if(err) {
+                        client.close();
+                        callback(err);
+                        return;
+                    }
+                    console.log('Result of find:\n', docs);
+                    client.close();
+                    callback(err);
+                });
+        });
     });
-  });
 }
 
-testWithCallbacks(function (err) {
-  if (err) {
-    console.log(err);
-  }
+async function testWithAsync() {
+    console.log('\n--- testWithAsync ---');
+    const client = new MongoClient(url, { useNewUrlParser: true });
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db();
+        const collection = db.collection('employees');
+
+        const employee = { id: 2, name: 'B. Async', age: 16 };
+        const result = await collection.insertOne(employee);
+        console.log('Result of insert:\n', result.insertedId);
+        const docs = await collection.find({ _id: result.insertedId })
+        .toArray();
+        console.log('Result of find:\n', docs);
+
+    }   catch(err) {
+        console.log(err);
+    }   finally {
+        client.close();
+    }
+}
+
+testWithCallbacks(function(err) {
+    if (err) {
+        console.log(err);
+    }
+    testWithAsync();
 });
