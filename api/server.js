e7f3e6c3bed6d1eafa5e7081132e03656f5bb548
Hitendra Quessou <sanjay_quessou@yahoo.co.uk>
2022-01-31 20:32:23 +0000

    Chapter 7. UI Server

--- api/server.js
+++ api/server.js
@@ -98,7 +98,7 @@ async function connectToDb(){
 }
 
 const server = new ApolloServer({
-    typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+    typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
     resolvers,
     formatError: error => {
         console.log(error);
@@ -108,8 +108,6 @@ const server = new ApolloServer({
 
 const app = express();
 
-app.use(express.static('public'));
-
 server.applyMiddleware({ app, path: '/graphql' });
 
 
@@ -117,7 +115,7 @@ server.applyMiddleware({ app, path: '/graphql' });
     try {
         await connectToDb();
         app.listen(3000, function(){
-        console.log('App started on port 3000');
+        console.log('AppAPI server started on port 3000');
         });
 } catch (err) {
     console.log('ERROR:', err);
